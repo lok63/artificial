c00c85dc3cab070ce004e4afdb0eef5ef3010c2d
leonidas constantinou <leonidas_kon94@hotmail.com>
2019-05-13 17:39:53 +0100

    add allowed hosts

--- artificial_demo/artificial_demo/settings.py
+++ artificial_demo/artificial_demo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '3h1+d!63dr0_0^+$et)+mtj*(w04eq+5-yqm)3j4kz%o6^#*sk'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['35.178.199.179']
 
 
 # Application definition
@@ -120,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
